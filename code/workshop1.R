46c389b5cc3926a458ae72be2df9cd75b8afb342
nicolinewinding <nicoline.winding@hotmail.com>
2024-05-15 10:11:10 +1000

    Wednesday morning second commit

--- code/workshop1.R
+++ code/workshop1.R
@@ -120,5 +120,99 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(mapping = aes(color = class)) + 
   geom_smooth(data = filter(mpg, class == "subcompact"), se = FALSE)
 
+###Exercise 
+##What geom would you use to draw a line chart? 
+#A boxplot? geom_boxplot()
+#A histogram? geom_histogram()
+#An area chart? geom_area()
+##Run the following code and try to predict if they will look differently. No I don't think they will.
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+ggplot() + 
+  geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
+
+### Transformations and Stats
+#here we will learn about easy transformations and data summaries
+
+## Plotting statistics
+#bar chart to show availability of different cut qualities
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut)) #the x axis displays a variable not in the data set, this is because geom_bar groups your data into counts by calculating statistical transformations
+#you can make the same exact figure by using a stat command instead
+ggplot(data = diamonds) + 
+  stat_count(mapping = aes(x = cut)) #because every geom has a default stat
+
+## Overriding defaults in ggplot2
+#change the default stat to an identity
+demo <- tribble(
+  ~cut,         ~freq,
+  "Fair",       1610,
+  "Good",       4906,
+  "Very Good",  12082,
+  "Premium",    13791,
+  "Ideal",      21551
+)
+demo
+
+ggplot(data = demo) +
+  geom_bar(mapping = aes(x = cut, y = freq), stat = "identity") #this overrides the default stat
+
+#you can also override a default mapping from transformed variables to aesthetics
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = stat(prop), group = 1)) #this shows propertion rather than count on the x axis
+
+## Plotting statistical details
+#showing uncertainty or limitations is good practice, by using stat_summary()
+ggplot(data = diamonds) + 
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.min = min,
+    fun.max = max,
+    fun = median
+  )
+
+### Aesthetic adjustments
+#colour and fill can also convey a lot of information
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, colour = cut))
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = cut))
+#you can also use aesthetics on specific variables
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity)) #this uses the position argument automatically
+#using 'position = "identity" will allow you to place each object excatly where it falls in the context of the graph
+ggplot(data = diamonds, mapping = aes(x = cut, fill = clarity)) + 
+  geom_bar(alpha = 1/5, position = "identity") #looks great
+
+ggplot(data = diamonds, mapping = aes(x = cut, colour = clarity)) + 
+  geom_bar(fill = NA, position = "identity") #bit confusing
+#using 'position = "fill" makes each stacked bar the same height and shows data as proportion of 1 instead
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "fill")
+#using 'position = "dodge" places overlapping objects beside each other rather than stacked
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "dodge")
+#using 'position = "jitter" adds a small amount of random noise to each point to avoid overplotting when points overlap
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), position = "jitter")
+
+### The layered grammar of graphics
+#template for making ggplots in the future
+#ggplot(data = <DATA>) + 
+#  <GEOM_FUNCTION>(
+#    mapping = aes(<MAPPINGS>),
+#    stat = <STAT>, 
+#    position = <POSITION>
+#  ) +
+#  <FACET_FUNCTION>
+
+
+### Plot deconstruction assignment
+
+
+
 
 
